9e4cff00fb740c12edf7eb183a60c836531cbbde
Eduardo Albertos G <albertos.eduardo@gmail.com>
2024-12-12 15:25:39 -0600

    Add authentication with api tokens

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate_request!
+
+  protected
+
+  def authenticate_request!
+    head(:forbidden) unless api_token
+  end
+
+  def api_token
+    @api_token ||= ApiToken.find_by(value: authorization_header)
+  end
+
+  def authorization_header
+    pattern = /^Bearer /
+    header = request.headers['Authorization']
+    header.gsub(pattern, '') if header&.match(pattern)
+  end
+
+  rescue_from ActionController::ParameterMissing do |e|
+    head(:bad_request)
+  end
 end
